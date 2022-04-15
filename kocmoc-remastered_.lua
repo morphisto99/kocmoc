68dbe4e3f29799007006611c14633b5ed84526e6
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-04-16 00:20:58 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -652,6 +652,7 @@ end
 
 -- Morphisto
 function killquestmobs(mobsname)
+	local mcount = 0
     for i,v in pairs(game:GetService("Workspace").MonsterSpawners:GetChildren()) do
         if v:FindFirstChild("Territory") then
             if v.Name:match(mobsname) and v.Name ~= "Commando Chick" and v.Name ~= "CoconutCrab" and v.Name ~= "StumpSnail" and v.Name ~= "TunnelBear" and v.Name ~= "King Beetle Cave" and not v.Name:match("CaveMonster") and not v:FindFirstChild("TimerLabel", true).Visible then
@@ -661,11 +662,10 @@ function killquestmobs(mobsname)
 					monsterpart = v.Territory.Value
 				end
                 api.humanoidrootpart().CFrame = monsterpart.CFrame
-				local count = 0
                 repeat api.humanoidrootpart().CFrame = monsterpart.CFrame avoidmob()
 					task.wait(1)
-					count = count + 1
-				until v:FindFirstChild("TimerLabel", true).Visible or not kocmoc.toggles.autofarm or count = 15
+					mcount = mcount + 1
+				until v:FindFirstChild("TimerLabel", true).Visible or not kocmoc.toggles.autofarm or mcount > 14
                 for i = 1, 4 do gettoken(monsterpart.Position) end
             end
         end
