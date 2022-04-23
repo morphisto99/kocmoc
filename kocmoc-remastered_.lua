464743dd5dd31f60ab68d9ad7c3bbc72befcfcf3
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-04-23 21:33:14 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -599,36 +599,6 @@ end
 
 function KillCoconutCrab()
 	local crabisready = false
-	[[--
-	for i,v in pairs(game:GetService("Workspace").MonsterSpawners:GetChildren()) do
-		if not string.find(v.Name,"CaveMonster") then
-			local mobText = nil
-			mobText = fetchVisualMonsterString(v)
-			if mobText ~= nil then
-				if mobText == "Coconut Crab: Ready" then
-					crabisready = true
-				end
-			end
-		end
-	end
-	if crabisready then
-		game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type="Demon Mask";Category="Accessory"})
-		disableall()
-		api.humanoidrootpart().CFrame = CFrame.new(-307.52117919922, 107.91863250732, 467.86791992188)
-		task.wait(5)
-		while game.Workspace.Monsters:FindFirstChild("Coconut Crab (Lvl 12)") and not temptable.started.vicious and not temptable.started.monsters do
-			task.wait(1)
-		end
-		api.tween(.5, CFrame.new(-259.4, 71.9, 462.1))
-		task.wait(1)
-		if kocmoc.toggles.autosprinkler then makesprinklers() end
-		for i = 0, 50 do
-			gettoken(CFrame.new(-259.4, 71.9, 462.1).Position)
-		end
-		enableall()
-		game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type=kocmoc.vars.defmask;Category="Accessory"}
-	end
-	]]--
 end
 -- Morphisto
 
