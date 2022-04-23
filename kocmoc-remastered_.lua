fb760ebf0a80a5e439252f18e4d75979329cdfe3
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-04-23 23:49:10 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2126,23 +2126,23 @@ function KillCoconutCrab()
 		end
 	end
 	if crabisready then
-		--print("It's ready")
-		game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type="Demon Mask";Category="Accessory"})
+		print("It's ready")
+		--game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type="Demon Mask";Category="Accessory"})
 		temptable.started.crab = true
-		disableall()
+		--disableall()
 		api.humanoidrootpart().CFrame = CFrame.new(-307.52117919922, 107.91863250732, 467.86791992188)
 		task.wait(10)
 		while game.Workspace.Monsters:FindFirstChild("Coconut Crab (Lvl 12)") and not temptable.started.vicious and not temptable.started.monsters do
 			task.wait(1)
 		end
-		api.tween(.5, CFrame.new(-259.4, 71.9, 462.1))
-		task.wait(1)
-		if kocmoc.toggles.autosprinkler then makesprinklers() end
-		for i = 0, 50 do
-			gettoken(CFrame.new(-259.4, 71.9, 462.1).Position)
-		end
-		enableall()
-		game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type=kocmoc.vars.defmask;Category="Accessory"}
+		--api.tween(.5, CFrame.new(-259.4, 71.9, 462.1))
+		--task.wait(1)
+		--if kocmoc.toggles.autosprinkler then makesprinklers() end
+		--for i = 0, 50 do
+			--gettoken(CFrame.new(-259.4, 71.9, 462.1).Position)
+		--end
+		--enableall()
+		--game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type=kocmoc.vars.defmask;Category="Accessory"}
 		temptable.started.crab = false
 	end
 end
