ceacc1e3cc73a2d978f8388a82aedf9c17e228f9
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-04-24 01:53:59 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2134,15 +2134,15 @@ function KillCoconutCrab()
 		while game.Workspace.Monsters:FindFirstChild("Coconut Crab (Lvl 12)") and not temptable.started.vicious and not temptable.started.monsters do
 			task.wait(1)
 		end
-		api.tween(1, CFrame.new(-259.4, 71.9, 462.1))
+		api.tween(0.5, CFrame.new(-259.4, 71.9, 462.1))
 		task.wait(1)
 		--if kocmoc.toggles.autosprinkler then makesprinklers() end
 		for i = 0, 50 do
 			gettoken(CFrame.new(-259.4, 71.9, 462.1).Position)
 		end
-		enableall()
 		temptable.started.crab = false
-		game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type=kocmoc.vars.defmask;Category="Accessory"}
+		enableall()
+		--game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Equip", {Mute=false;Type=kocmoc.vars.defmask;Category="Accessory"}
 	end
 end
 -- Morphisto
