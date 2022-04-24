d26eaf4a066e460ccf5198adcc3e4aabe94cd2c7
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-04-25 01:44:58 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2355,13 +2355,13 @@ function KillTunnelBear()
 		task.wait(15)
 		--api.humanoidrootpart().CFrame = CFrame.new(350.4, 25.7, -39.41)
 		api.humanoidrootpart().CFrame = CFrame.new(400.4, 6.783, -39.41)
-		task.wait(3)
+		task.wait(5)
 		--if GetItemListWithValue()["Stinger"] > 0 then
 			--game:GetService("ReplicatedStorage").Events.PlayerActivesCommand:FireServer({["Name"] = "Stinger"})
 		--end
 		
 		for i = 0, 20 do
-			api.humanoidrootpart().CFrame = CFrame.new(350.4128112792969, 27.783041000366211, -39.41004943847656)
+			api.humanoidrootpart().CFrame = CFrame.new(350.4128112792969, 29.783041000366211, -39.41004943847656)
 			temptable.float = true
 			task.wait(1)
 		end
@@ -2371,7 +2371,7 @@ function KillTunnelBear()
 			--mondopition = game.Workspace.Monsters["Tunnel Bear (Lvl 9)"].Head.Position
 			--api.tween(.5, CFrame.new(mondopition.x, mondopition.y + 13, mondopition.z))
 			--api.humanoidrootpart().CFrame = CFrame.new(mondopition.x, mondopition.y + 13, mondopition.z)
-			api.humanoidrootpart().CFrame = CFrame.new(283.4128112792969, 27.783041000366211, -39.41004943847656)
+			api.humanoidrootpart().CFrame = CFrame.new(283.4128112792969, 29.783041000366211, -39.41004943847656)
 			temptable.float = true
 			task.wait(1)
 		end
