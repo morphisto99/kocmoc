d681029cb82a9512b7a4baa4dd89600035644da7
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-05-01 00:24:55 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2589,8 +2589,8 @@ function KillStumpSnail()
 		else
 			while game.Workspace.Monsters:FindFirstChild("Stump Snail (Lvl 6)") and not temptable.started.vicious and not temptable.started.monsters do
 				game:GetService("Workspace").Map.Ground.HighBlock.CanCollide = false
-				snailposition = game.Workspace.Monsters["Stump Snail (Lvl 6)"].Head.Position
-				api.tween(1, CFrame.new(snailposition.x, snailposition.y - 40, snailposition.z))
+				--snailposition = game.Workspace.Monsters["Stump Snail (Lvl 6)"].Head.Position
+				--api.tween(1, CFrame.new(snailposition.x, snailposition.y - 40, snailposition.z))
 				task.wait(1)
 			end
 			task.wait(0.5)
