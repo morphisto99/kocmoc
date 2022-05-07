a24045806eb20e31f203e62a26ee1cfdf2487d76
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-05-07 14:18:01 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2633,7 +2633,7 @@ function KillTest()
 
 	print('test0')
 	-- for i,v in pairs(game.Workspace.Happenings.Puffshrooms.PuffballMushroomModelCommon:GetChildren()) do
-	for i,v in pairs(game.Workspace.Happenings:GetChildren()) do
+	for i,v in pairs(game.Workspace.Happenings.Puffshrooms:GetChildren()) do
 		print('v.Name=' .. v.Name)
 		fieldpos1 = api.partwithnamepart("Rare", game.Workspace.Happenings.Puffshrooms):FindFirstChild("Puffball Stem").CFrame
 		print('Found Rare pos:' .. fieldpos1)
