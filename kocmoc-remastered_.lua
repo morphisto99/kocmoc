e0496be6fde6c64aba48395c75b62ae179e8b7dd
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-05-08 23:37:03 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2628,8 +2628,8 @@ function KillTest2()
 		--print('Player=' .. v.Name)
 		--game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace[v.Name].HumanoidRootPart.CFrame - teleport to player
 		for j,k in next, kocmoc.wlplayers do
-			--print('k=' .. k .. ' v.Name=' .. v.Name)
-			if k ~= v.Name then
+			print('k=' .. k .. ' v.Name=' .. v.Name)
+			if k ~= tostring(v.Name) then
 				--allwlplayer = true
 				local playerpos = game.Workspace:FindFirstChild(v.Name).HumanoidRootPart.Position
 				print(v.Name .. ': x=' .. playerpos.X .. ' y=' .. playerpos.Y .. ' z=' .. playerpos.Z)
