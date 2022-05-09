88860aa1687a16ac1fd9e460e0ffb5e35be6ea9f
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-05-09 22:47:22 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2322,7 +2322,7 @@ task.spawn(function()
     end
 	
 	for i, v in pairs(game.Players:GetChildren()) do
-		wlplayers:CreateButton('Player' .. i .. ': ' .. v, function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace[v].HumanoidRootPart.CFrame end)
+		wlplayers:CreateButton('Player' .. i .. ': ' .. v.Name, function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace[v.Name].HumanoidRootPart.CFrame end)
 	end
 		
     local mob2 = panel:CreateButton("Mondo Chick: 00:00",function() api.tween(1,game:GetService("Workspace").FlowerZones["Mountain Top Field"].CFrame) end)
