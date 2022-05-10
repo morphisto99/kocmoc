a1758422b3fed5087cbaf1169aa3f804d9fd7ec9
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-05-11 01:07:16 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2643,7 +2643,7 @@ function KillTest2()
 	local count = 1
 	for i, v in pairs(game:GetService("CoreGui"):GetDescendants()) do
 		if v:IsA("TextLabel") and string.find(v.Text,"Player" .. count) then
-			v.Parent.Parent:Destroy()
+			v.Parent:Destroy()
 			if count > 6 then
 				break
 			else
@@ -2659,6 +2659,7 @@ function KillTest2()
 			local playerpos = game.Workspace:FindFirstChild(v.Name).HumanoidRootPart.Position
 			if (playerpos-game.Players.LocalPlayer.Character.HumanoidRootPart.Position).magnitude < 150 then
 				uiplayersinrange:CreateLabel('Player' .. i .. ': ' .. v.Name .. ' in range!')
+				print('Player' .. i .. ': ' .. v.Name .. ' in range!')
 			end
 		else
 			uiwlplayers:CreateButton('Player' .. i .. ': ' .. v.Name, function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace:FindFirstChild(v.Name).HumanoidRootPart.CFrame end)
