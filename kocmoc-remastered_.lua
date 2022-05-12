c3d4c19cb067e5c696754c91ee1fa7b5ad5bf2e3
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-05-13 01:17:04 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -2644,9 +2644,9 @@ function KillTest2()
 		--end
 	--end
 	
-	for i,v in next, game:GetService("Players").LocalPlayer.PlayerGui.Frame:GetDescendants() do
-		print('1=' .. v.Name)
-	end
+	--for i,v in next, game:GetService("Players").LocalPlayer.PlayerGui:GetDescendants() do
+		--print('1=' .. v.Name)
+	--end
 
 	for i,v in next, game:GetService("Players").LocalPlayer.PlayerGui.UI:GetDescendants() do
 		print('2=' .. v.Name)
