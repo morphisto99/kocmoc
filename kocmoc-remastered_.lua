d411f651bce38e792c7954dc9712277464cbed2a
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-05-29 17:14:41 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -3127,12 +3127,6 @@ function KillTest2()
 							for b,z in next, getconnections(button) do
 								z.Function()
 							end
-							local ScreenGui = game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui
-							for x = 1, 7 do
-								print('inside of ScreenGui')
-								firesignal(ScreenGui.NPC.ButtonOverlay.MouseButton1Click)
-								task.wait(1)
-							end
 							--local option3 = game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.NPC.OptionFrame:FindFirstChild("Option3")
 							--print(option3.Text)
 							--[[
@@ -3149,10 +3143,18 @@ function KillTest2()
     end
 	
 	print("Test2.1")
-	local option3 = game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.NPC.OptionFrame:FindFirstChild("Option1")
+
+	local ScreenGui = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("ScreenGui")
+	for x = 1, 7 do
+		firesignal(ScreenGui.NPC.ButtonOverlay.MouseButton1Click)
+		task.wait(1)
+	end
+	
+	print("Test2.2")
+	local option3 = game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.NPC.OptionFrame:FindFirstChild("Option3")
 	if option3.Text == "Cancel" then
 		print('Inside of Option3')
-		option3.ActivateButton.MouseButton1Click
+		firesignal(option3.ActivateButton.MouseButton1Click)
 	end	
 	--word=str:match("%((%a+)%)") -- get strings in ( )
 	
