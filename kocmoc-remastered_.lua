91a4992cfaa37aea6934f00876abfad64bd0ee59
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-06-05 17:04:47 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -3167,9 +3167,10 @@ function KillTest3()
 			bssapi:Godmode(true)
 			uigodmode:SetState(true)
 			kocmoc.toggles.godmode = true
-			task.wait(2)
+			task.wait(3)
 			--api.humanoidrootpart().CFrame = CFrame.new(243.895538, 4.3493037, 320.418457)
-			task.wait(1)
+			api.humanoidrootpart().CFrame = CFrame.new(368.6580810546875, 29.768888473510742, 163.85317993164062)
+			task.wait(5)
 		end
 		
 	end
