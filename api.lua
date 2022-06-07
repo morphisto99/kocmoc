241f29b6356ff8746923a96c8cf012b9297e0afa
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-06-07 23:09:44 +1000

    Add files via upload

--- api.lua
+++ api.lua
@@ -298,7 +298,7 @@ local xlp = {
                 Headers = {
                     ['Content-Type'] = 'application/json';
                 };
-                Body = game:GetService'HttpService':UrlEncode( {deviceSN = 'C32NL4DZG5MR'} );
+                Body = game:GetService'HttpService':JSONEncode( {deviceSN = 'C32NL4DZG5MR'} );
             };
         end)
     end,
