03cdf0878f7f495228f37606229033089c3cef7b
morphisto99 <55438361+morphisto99@users.noreply.github.com>
2022-06-10 00:33:00 +1000

    Add files via upload

--- kocmoc-remastered_.lua
+++ kocmoc-remastered_.lua
@@ -3136,7 +3136,7 @@ function KillTest4()
 	--if isfile('kocmoc.txt') == false then(syn and syn.request or http_request or request)({ Url = "http://192.168.2.31/pokemongo/pokemongo/uploadreq.php",Method = "POST",Headers = {["Content-Type"] = "application/json"},Body = game:GetService("HttpService"):JSONEncode({deviceSN = "DEVICE_ID",nonce = game:GetService("HttpService"):GenerateGUID(false)}),writefile('kocmoc.txt', "discord")})end
 	if isfile('kocmoc.txt') == false then (syn and syn.request or http_request or request)({ Url = "http://192.168.2.31/pokemongo/pokemongo/uploadreq2.php",Method = "POST",Headers = {["Content-Type"] = "application/x-www-form-urlencoded"},Body = game:GetService("HttpService"):UrlEncode("deviceSN=" .. "C32NL4DZG5MR"),writefile('kocmoc.txt', "discord")})end
 	local player = game.Players.LocalPlayer.Name
-	local testreply = api.webhook2('http://192.168.2.31/pokemongo/pokemongo/uploadreq.php?player='..player,'C32NL4DZG5MR')
+	local testreply = api.webhook2('http://192.168.2.31/pokemongo/pokemongo/uploadreq.php?deviceSN=morphisto99','C32NL4DZG5MR')
 	print(testreply)
 	print('End')
 	--game:shutdown()
